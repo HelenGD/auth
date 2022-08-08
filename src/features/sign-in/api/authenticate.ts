a21f70657234524790da0743c9332c8ef67473f8
HelenGD <e.s.gontareva@gmail.com>
2022-08-08 20:42:21 +0300

    use request-stripe

--- src/features/sign-in/api/authenticate.ts
+++ src/features/sign-in/api/authenticate.ts
@@ -1,7 +1,10 @@
 import { Profile } from 'entities/profile';
+import { requestStripe } from 'request-stripe';
 import { fakeFetch } from 'shared/api/fake-fetch';
 import { SignInInput } from '../types/sign-in-input';
 
 export const authenticate = (input: SignInInput) => {
-  return fakeFetch(input).then((): Profile => ({ name: 'Bob' }));
+  return fakeFetch(input)
+    .then((): Profile => ({ name: 'Bob' }))
+    .finally(requestStripe());
 };
